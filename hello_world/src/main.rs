a2278214d69b185a1dcf8fd036e086cf4130c23f
Bartek <wlodarcykb16@gmail.com>
2026-07-21 15:56:07 +0200

    Fix calorie calculation and correct function parameter usage in main logic

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -7,10 +7,10 @@ fn main() {
     let kalorie: f64 = oblicz_kalorie(waga);
     
     let gramy_bialka: f64 = bialko_gramy(waga);
-    let kcal_b: f64 = bialko_kcal(bialko_gramy);
+    let kcal_b: f64 = bialko_kcal(gramy_bialka);
 
     let tluszcze_gramy: f64 = tluszcze_gramy(waga);
-    let tluszcze_kcal: f64 = tluszcze_kcal();
+    let tluszcze_kcal: f64 = tluszcze_kcal(tluszcze_gramy);
 
     println!("Twoje zapotrzebowanie na białko wynosi {}g oraz zapotrzebowanie na kcal wynosi {}kcal", bialko, kalorie);
     
@@ -22,6 +22,6 @@ fn bialko_gramy(waga: f64) -> f64 {waga * 2.0}
 fn bialko_kcal(gramy_bialka: f64) -> f64 {gramy_bialka * 4.0}
 
 fn tluszcze_gramy(waga: f64) -> f64{waga * 1.0}
-fn tluszcze_kcal(tluszcze_gramy: f64) -> f64 {tluszcze gramy * 9.0}
+fn tluszcze_kcal(tluszcze_gramy: f64) -> f64 {tluszcze_gramy * 9.0}
 
 fn pula_na_wegle(waga: f64) -> f64{oblicz_kalorie(waga) - 
