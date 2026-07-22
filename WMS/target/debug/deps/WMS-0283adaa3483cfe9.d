429e394a6de1829e4ab13fa79586484556f2b321
Bartek <wlodarcykb16@gmail.com>
2026-07-22 15:14:21 +0200

    Add incremental build artifacts and fix warnings in WMS project
    - Created new binary work-products file for incremental builds.
    - Added lock file for dependency management.
    - Captured stderr output during build, highlighting fingerprint errors due to missing files.
    - Captured stdout output, including warnings about unused fields in the `Towar` struct.

---
+++ WMS/target/debug/deps/WMS-0283adaa3483cfe9.d
@@ -0,0 +1,5 @@
+C:\Users\Bartek\Desktop\rust\WMS\target\debug\deps\WMS-0283adaa3483cfe9.d: src\main.rs
+
+C:\Users\Bartek\Desktop\rust\WMS\target\debug\deps\libWMS-0283adaa3483cfe9.rmeta: src\main.rs
+
+src\main.rs:
---
+++ WMS/target/debug/deps/WMS-65a3705eecb0a7cf.d
@@ -0,0 +1,5 @@
+C:\Users\Bartek\Desktop\rust\WMS\target\debug\deps\WMS-65a3705eecb0a7cf.d: src\main.rs
+
+C:\Users\Bartek\Desktop\rust\WMS\target\debug\deps\libWMS-65a3705eecb0a7cf.rmeta: src\main.rs
+
+src\main.rs:
