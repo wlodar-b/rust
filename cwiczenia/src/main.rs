8d10ff664ab597144aca4b4694b1d547b5aaef71
Bartek <wlodarcykb16@gmail.com>
2026-07-22 12:18:00 +0200

    Add incremental build artifacts for cwiczenia project
    - Created new binary files: query-cache.bin and work-products.bin
    - Added lock file for incremental build management

--- cwiczenia/src/main.rs
+++ cwiczenia/src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
     let mut liczby: Vec<i32> = Vec::new();
 
-    liczby.push(10);
-    liczby.push(20);
-    liczby.push(30);
-    
+    liczby.push(450);
+    liczby.push(600);
+    liczby.push(320);
+
+    println!("Na liście znajduje się {} posiłki", liczby.len());
 }
