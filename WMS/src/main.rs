fd964272c80fbc9ce3dc3d1417acb819a5f9ed41
Bartek <wlodarcykb16@gmail.com>
2026-07-23 10:06:51 +0200

    Add work-products and lock files for incremental build
    - Created a new binary file `work-products.bin` containing version information.
    - Added a lock file to manage dependencies for the incremental build process.

--- WMS/src/main.rs
+++ WMS/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
 
     loop {
 
-        println!("=========================================\nMAGAZYN ZWROTÓW KONSUMENCKICH\n=========================================\\\nWybierz operację:\n[1] - Dodaj nowy towar z palety\n[2] - Wyszukaj i wyświetl produkt\n[0] - Wyjście / Zamknij program\n=========================================");
+        println!("=========================================\nMAGAZYN ZWROTÓW KONSUMENCKICH\n=========================================\nWybierz operację:\n[1] - Dodaj nowy towar do magazynu\n[2] - Wyszukaj i wyświetl produkt\n[0] - Wyjście \n=========================================");
         print!("Twój wybór: ");
         io::stdout().flush().expect("Błąd");
 
@@ -67,6 +67,101 @@ fn main() {
                 }
 
                 2 => {
+                    loop {
+                        print!(" [1] - Szukaj po marce\n [2] - Szukaj po rozmiarze\n [3] - Szukaj po stanie\n [0] - Powrót do głównego menu\n");
+                        print!("Twój wybór: ");
+                        io::stdout().flush().expect("Błąd");
+
+                        let mut wejscie_podmenu = String::new();
+                        io::stdin().read_line(&mut wejscie_podmenu).expect("Błąd odczytu");
+                        // Parsowanie na liczbe
+                        let wybor2: i32 = wejscie_podmenu.trim().parse().expect("To nie jest liczba!");
+
+                        match wybor2 {
+                            1 => {
+                                // Pobieramy dane od użytkownika i czyścimy je
+                                print!("Podaj marke której szukasz: ");
+                                io::stdout().flush().expect("Błąd");
+                                let mut wpisana_marka = String::new();
+                                io::stdin().read_line(&mut wpisana_marka).expect("Błąd");
+                                let szukana_marka = wpisana_marka.trim().to_string();
+                                
+                                // Flaga Pomocnicza
+                                let mut znaleziono = false;
+                                println!("--- WYNIKI WYSZUKIWANIA ---");
+
+                                // Pętla przechodzaca przez magazyn
+                                for przedmiot in &magazyn {
+                                    // Porównujemy tekst z wektora z tekstem wpisanym przez Ciebie
+                                    if przedmiot.marka == szukana_marka {
+                                        println!("ID: [{}] | {} | Rozmiar: {} | Stan: {}", przedmiot.id, przedmiot.marka, przedmiot.rozmiar, przedmiot.stan);
+
+                                        znaleziono = true;
+                                    }
+                                }
+                                // Komunikat jeżeli nic nie znaleziono
+                                if znaleziono == false {
+                                    println!("Nie znaleziono w magazynie towaru tej marki");
+                                }
+                            }
+                            2 => {
+                                // Pobieramy dane od użytkownika i czyścimy je
+                                print!("Podaj rozmiar który szukasz: ");
+                                io::stdout().flush().expect("Błąd");
+                                let mut wpisany_rozmiar = String::new();
+                                io::stdin().read_line(&mut wpisany_rozmiar).expect("Błąd");
+                                // Zamieniamy na i32
+                                let szukany_rozmiar: i32 = wpisany_rozmiar.trim().parse().expect("To nie jest liczba!");
+
+                                let mut znaleziono = false;
+                                println!("--- WYNIKI WYSZUKIWANIA ---");
+
+                                for przedmiot in &magazyn {
+                                    if przedmiot.rozmiar == szukany_rozmiar {
+                                        println!("ID: [{}] | {} | Rozmiar: {} | Stan: {}", przedmiot.id, przedmiot.marka, przedmiot.rozmiar, przedmiot.stan);
+
+                                        znaleziono = true;
+                                    }
+                                }
+                                if znaleziono == false {
+                                    println!("Nie znaleziono w magazynie towaru w tym rozmiarze");
+                                }
+                            }
+                            3 => {
+                                print!("Podaj stan który szukasz: ");
+                                io::stdout().flush().expect("Błąd");
+                                let mut wpisany_stan = String::new();
+                                io::stdin().read_line(&mut wpisany_stan).expect("Błąd");
+                                let szukany_stan = wpisany_stan.trim().to_string();
+                                
+                                // Flaga Pomocnicza
+                                let mut znaleziono = false;
+                                println!("--- WYNIKI WYSZUKIWANIA ---");
+
+                                // Pętla przechodzaca przez magazyn
+                                for przedmiot in &magazyn {
+                                    // Porównujemy tekst z wektora z tekstem wpisanym przez Ciebie
+                                    if przedmiot.stan == szukany_stan {
+                                        println!("ID: [{}] | {} | Rozmiar: {} | Stan: {}", przedmiot.id, przedmiot.marka, przedmiot.rozmiar, przedmiot.stan);
+
+                                        znaleziono = true;
+                                    }
+                                }
+                                // Komunikat jeżeli nic nie znaleziono
+                                if znaleziono == false {
+                                    println!("Nie znaleziono w magazynie towaru w tym stanie.");
+                                }
+                            }
+
+                            0 => {
+                                println!("Powrót do głównego menu!");
+                                break;
+                            }
+                            _ => {
+                                println!("Nieprawidłowy wybór wpisz 1, 2, 3 lub 0");
+                            }
+                        }
+                    }
 
 
                 }
@@ -80,14 +175,4 @@ fn main() {
                 
             }
         } 
-
-
-
-
-
-
-
-
-        
-
 }
