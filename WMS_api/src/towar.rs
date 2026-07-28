cfb25010b2ec1dbad8f9a2fae9f3cba744ef4c94
Bartek <wlodarcykb16@gmail.com>
2026-07-28 09:36:51 +0200

    Add Towar struct to define item properties

---
+++ WMS_api/src/towar.rs
@@ -0,0 +1,10 @@
+pub struct Towar {     
+    pub nazwa: String, 
+    pub marka: String,
+    pub kolor: String,
+    pub rozmiar: i32,
+    pub stan: StanTowaru,
+    pub id: i32,
+    pub cena_zakupu: f32,
+    pub cena_sprzedazy: f32,
+}
